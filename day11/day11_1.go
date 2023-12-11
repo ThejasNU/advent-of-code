4c56448414a3a2a69b80050c025fe7e4cab4d329
ThejasNU <thejasnu@gmail.com>
2023-12-11 22:19:45 +0530

    day11 format

--- day11/day11_1.go
+++ day11/day11_1.go
@@ -7,45 +7,45 @@ import (
 	"github.com/ThejasNU/advent-of-code/helpers"
 )
 
-func Day11_1(){
-	input:=helpers.ReadInput("./day11/day11_input.txt")
-	galaxyIdxs:=getGalaxyIdxs(input)
-	emptyRows:=getEmptyRows(input)
-	emptyCols:=getEmptyCols(input)
+func Day11_1() {
+	input := helpers.ReadInput("./day11/day11_input.txt")
+	galaxyIdxs := getGalaxyIdxs(input)
+	emptyRows := getEmptyRows(input)
+	emptyCols := getEmptyCols(input)
 
-	ans:=0
-	n:=len(galaxyIdxs)
-	for i:=0;i<n;i++{
-		for j:=i+1;j<n;j++{
-			dist:=int(math.Abs(float64(galaxyIdxs[j][0]-galaxyIdxs[i][0])))+int(math.Abs(float64(galaxyIdxs[j][1]-galaxyIdxs[i][1])))
-			minR:=int(math.Min(float64(galaxyIdxs[i][0]),float64(galaxyIdxs[j][0])))
-			maxR:=int(math.Max(float64(galaxyIdxs[i][0]),float64(galaxyIdxs[j][0])))
-			minC:=int(math.Min(float64(galaxyIdxs[i][1]),float64(galaxyIdxs[j][1])))
-			maxC:=int(math.Max(float64(galaxyIdxs[i][1]),float64(galaxyIdxs[j][1])))
+	ans := 0
+	n := len(galaxyIdxs)
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			dist := int(math.Abs(float64(galaxyIdxs[j][0]-galaxyIdxs[i][0]))) + int(math.Abs(float64(galaxyIdxs[j][1]-galaxyIdxs[i][1])))
+			minR := int(math.Min(float64(galaxyIdxs[i][0]), float64(galaxyIdxs[j][0])))
+			maxR := int(math.Max(float64(galaxyIdxs[i][0]), float64(galaxyIdxs[j][0])))
+			minC := int(math.Min(float64(galaxyIdxs[i][1]), float64(galaxyIdxs[j][1])))
+			maxC := int(math.Max(float64(galaxyIdxs[i][1]), float64(galaxyIdxs[j][1])))
 
-			for _,emptyRow:=range emptyRows{
-				if minR<=emptyRow && emptyRow<=maxR{
+			for _, emptyRow := range emptyRows {
+				if minR <= emptyRow && emptyRow <= maxR {
 					dist++
 				}
 			}
-			for _,emptyCol:=range emptyCols{
-				if minC<=emptyCol && emptyCol<=maxC{
+			for _, emptyCol := range emptyCols {
+				if minC <= emptyCol && emptyCol <= maxC {
 					dist++
 				}
 			}
-			ans+=dist
+			ans += dist
 		}
 	}
 	fmt.Println(ans)
 }
 
-func getGalaxyIdxs(input []string) [][]int{
+func getGalaxyIdxs(input []string) [][]int {
 	var galaxyIdxs [][]int
 
-	for i,row:=range input{
-		for j,char:=range row{
-			if char=='#'{
-				galaxyIdxs = append(galaxyIdxs, []int{i,j})
+	for i, row := range input {
+		for j, char := range row {
+			if char == '#' {
+				galaxyIdxs = append(galaxyIdxs, []int{i, j})
 			}
 		}
 	}
@@ -53,37 +53,37 @@ func getGalaxyIdxs(input []string) [][]int{
 	return galaxyIdxs
 }
 
-func getEmptyRows(input []string) []int{
-	m,n:=len(input),len(input[0])
+func getEmptyRows(input []string) []int {
+	m, n := len(input), len(input[0])
 	var emptyRows []int
-	for i:=0;i<m;i++{
-		isEmpty:=true
-		for j:=0;j<n;j++{
-			if rune(input[i][j])!='.'{
-				isEmpty=false
+	for i := 0; i < m; i++ {
+		isEmpty := true
+		for j := 0; j < n; j++ {
+			if rune(input[i][j]) != '.' {
+				isEmpty = false
 				break
 			}
 		}
 
-		if isEmpty{
+		if isEmpty {
 			emptyRows = append(emptyRows, i)
 		}
 	}
 	return emptyRows
 }
 
-func getEmptyCols(input []string) []int{
-	m,n:=len(input),len(input[0])
+func getEmptyCols(input []string) []int {
+	m, n := len(input), len(input[0])
 	var emptyCols []int
-	for i:=0;i<n;i++{
-		isEmpty:=true
-		for j:=0;j<m;j++{
-			if rune(input[j][i])!='.'{
-				isEmpty=false
+	for i := 0; i < n; i++ {
+		isEmpty := true
+		for j := 0; j < m; j++ {
+			if rune(input[j][i]) != '.' {
+				isEmpty = false
 				break
 			}
 		}
-		if isEmpty{
+		if isEmpty {
 			emptyCols = append(emptyCols, i)
 		}
 	}
